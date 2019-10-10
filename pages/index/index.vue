94977b487f7ee1fb0781d5652484f7164579b38d
liuxy <ccliuxy@gmail.com>
2019-10-10 23:32:52 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<view>
+		<!-- <view class="page-body">
+			<view class="page-section page-section-gap">
+				<map style="width: 750upx;height: 2000upx;" :latitude="latitude" :longitude="longitude" :markers="covers">
+				</map>
+			</view>
+		</view> -->
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
